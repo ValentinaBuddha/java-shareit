bc5f118bc8aaf6a555b971b300edb7848d49b23c
Valentina <valentina.buddha@yandex.ru>
2023-05-06 20:29:03 +0300

    java-shareit sprint 13

--- src/main/java/ru/practicum/shareit/exception/NotUniqueEmailException.java
+++ src/main/java/ru/practicum/shareit/exception/NotUniqueEmailException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class NotUniqueEmailException extends RuntimeException{
+public class NotUniqueEmailException extends RuntimeException {
     public NotUniqueEmailException(String message) {
         super(message);
     }
