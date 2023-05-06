6faa6c6e8f968d132d4ae42e364c70e6c1aa432c
Valentina <valentina.buddha@yandex.ru>
2023-05-06 21:42:35 +0300

    java-shareit sprint 13

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -7,8 +7,6 @@ import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.item.dto.ItemDto;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Slf4j
